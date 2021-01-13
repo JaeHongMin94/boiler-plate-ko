21b372bad9728b6256ed7cbe7ddcc1612803cd33
JaeHongMin94 <moonshin123@naver.com>
2021-01-14 01:26:51 +0900

    로그인 로그아웃 회원가입

--- client/src/components/views/LoginPage/LiginPag.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function LiginPag() {
-  return <div>LoginPage</div>;
-}
-
-export default LiginPag;
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,49 +1,34 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { registerUser } from '../../../_action/user_action';
+import { loginUser } from '../../../_action/user_action';
 import { withRouter } from 'react-router-dom';
 
-function RegisterPage(props) {
+function LoginPage(props) {
   const dispatch = useDispatch();
-
   const [Email, setEmail] = useState('');
-  const [Name, setName] = useState('');
   const [Password, setPassword] = useState('');
-  const [ConfirmPassword, setConfirmPassword] = useState('');
 
   const onEmailHandler = (event) => {
     setEmail(event.currentTarget.value);
-  };
-
-  const onNameHandler = (event) => {
-    setName(event.currentTarget.value);
   };
 
   const onPasswordHandler = (event) => {
     setPassword(event.currentTarget.value);
   };
 
-  const onConfirmPasswordHandler = (event) => {
-    setConfirmPassword(event.currentTarget.value);
-  };
-
   const onSubmitHandler = (event) => {
     event.preventDefault();
-
-    if (Password !== ConfirmPassword) {
-      return alert('비밀번호와 비밀번호 확인은 같아야 합니다.');
-    }
 
     let body = {
       email: Email,
       password: Password,
-      name: Name,
     };
-    dispatch(registerUser(body)).then((response) => {
-      if (response.payload.success) {
-        props.history.push('/login');
+
+    dispatch(loginUser(body)).then((response) => {
+      if (response.payload.loginSuccess) {
+        props.history.push('/');
       } else {
-        alert('Failed to sign up');
+        alert('Error');
       }
     });
   };
@@ -64,25 +49,13 @@ function RegisterPage(props) {
       >
         <label>Email</label>
         <input type='email' value={Email} onChange={onEmailHandler} />
-
-        <label>Name</label>
-        <input type='text' value={Name} onChange={onNameHandler} />
-
         <label>Password</label>
         <input type='password' value={Password} onChange={onPasswordHandler} />
-
-        <label>Confirm Password</label>
-        <input
-          type='password'
-          value={ConfirmPassword}
-          onChange={onConfirmPasswordHandler}
-        />
-
         <br />
-        <button type='submit'>회원 가입</button>
+        <button type='submit'>Login</button>
       </form>
     </div>
   );
 }
 
-export default withRouter(RegisterPage);
+export default withRouter(LoginPage);
